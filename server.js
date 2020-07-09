c6e6f669682561f841d30a1f338fdc62c4d3aa03
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-07-09 10:27:42 -0600

    add server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var path = require ("path")
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//Here goes the JS code
