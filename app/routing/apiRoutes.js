68a218aef19aed88e8755f4ac27a79a96b84253f
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-07-10 12:31:35 -0600

    update apiRoutes. js with base code

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,18 @@
+var friendsArray = require("../data/friends");
+
+module.exports = function(app) {
+
+    app.get("/api/firends", function(req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res) {
+
+        if (friends.length)
+    })
+
+
+
+};
+
+
