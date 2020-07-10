31f8451aa55438304211bf77353445b268d876a4
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-07-10 13:27:54 -0600

    add to friendsArray

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,5 +13,96 @@ var friendsArray = [{
         4,
         1  
     ]
+
+},{
+    "name": "Lily",
+    "photo": "https://vetstreet.brightspotcdn.com/dims4/default/354d0cf/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fdc%2Fc4%2F8ccd3a28438d81b2f2f5d8031a05%2Fpug-ap-r82p3q-645.jpg",
+    "scores": [
+        4,
+        2,
+        1,
+        1,
+        1,
+        2,
+        1,
+        3,
+        5,
+        5  
+    ]
+},{
+    "name": "Rory",
+    "photo": "https://generated.photos/vue-static/home/feed/latino-male.png",
+    "scores": [
+        3,
+        3,
+        3,
+        3,
+        3,
+        4,
+        1,
+        1,
+        5,
+        2  
+    ]
+},{
+    "name": "Bob",
+    "photo": "https://www.randomlists.com/img/people/tyler_perry.webp",
+    "scores": [
+        3,
+        3,
+        1,
+        2,
+        1,
+        5,
+        4,
+        3,
+        1,
+        1  
+    ]
+},{
+    "name": "Wendy",
+    "photo": "https://cdnb.artstation.com/p/assets/images/images/017/756/039/large/sasha-a-.jpg?1557233380",
+    "scores": [
+        2,
+        1,
+        3,
+        5,
+        5,
+        4,
+        3,
+        5,
+        4,
+        1  
+    ]
+},{
+    "name": "Kennilda",
+    "photo": "https://dailylolpics.com/wp-content/uploads/2017/11/artist-transforms-photos-of-random-people-into-creative-illustrations.jpg",
+    "scores": [
+        5,
+        1,
+        1,
+        2,
+        2,
+        2,
+        4,
+        2,
+        1,
+        5  
+    ]
+},{
+    "name": "Broomhilda",
+    "photo": "https://photos.lensculture.com/large/5dd2fa6e-fe8d-469f-959b-46299ced511d.jpg",
+    "scores": [
+        1,
+        5,
+        5,
+        5,
+        5,
+        4,
+        4,
+        5,
+        4,
+        2  
+    ]
 }
 ]
--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express = require("express");
 var path = require ("path")
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-//Here goes the JS code
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
